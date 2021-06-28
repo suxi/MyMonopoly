fff59edcba663e077eec9b0b2bf8fdb68a775b7e
苏曦 <suxi@xiaohulu.com>
2021-06-28 15:24:39 +0800

    Initial Commit

---
+++ MyMonopoly/MyMonopolyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMonopolyApp.swift
+//  MyMonopoly
+//
+//  Created by 苏曦 on 2021/6/28.
+//
+
+import SwiftUI
+
+@main
+struct MyMonopolyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
